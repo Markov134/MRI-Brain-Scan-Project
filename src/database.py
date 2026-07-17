21bddf3db329753f6ce477b9e8f2ed34617e4a53
Markov134 <stephanmarokvski@gmail.com>
2026-07-17 08:53:51 +0200

    Added duplciation check and comments to the file.

--- src/database.py
+++ src/database.py
@@ -2,16 +2,23 @@ import sqlite3
 import os
 from PIL import Image
 
-dataset_path = 'data/raw/Brain-Tumor-MRI-Dataset'
+# ----------------------------
+# NOTE: Make sure to close the database connection after using these functions.
+# ----------------------------
+
+DATASET_PATH = 'data/raw/Brain-Tumor-MRI-Dataset'
+DATABASE_PATH = 'data/database/mri_dataset.db'
 
 def connect_db():
     # Makes a database if there isnt one already
-    return sqlite3.connect('data/database/mri_dataset.db')
+    return sqlite3.connect(DATABASE_PATH)
 
 def create_tables(conn):
 
+    # A Cursor is an object used to execute SQL queries on an SQLite database
     cursor = conn.cursor()
 
+    # Creates table inside the database
     cursor.execute("""CREATE TABLE IF NOT EXISTS mri_images (
         id INTEGER PRIMARY KEY,
         filename TEXT NOT NULL,
@@ -26,14 +33,20 @@ def create_tables(conn):
 
     conn.commit()
 
-    conn.close()
-
 def populate_database(conn):
 
     cursor = conn.cursor()
+
+    # Prevents duplicates by checking if the database is populated
+    cursor.execute("SELECT COUNT(*) FROM mri_images")
+
+    if cursor.fetchone()[0] > 0:
+        print("Database already populated.")
+        return
     
-    for split in os.listdir(dataset_path):
-        split_folder = os.path.join(dataset_path, split)
+    # This goes through the dataset and assigns the values inside the database
+    for split in os.listdir(DATASET_PATH):
+        split_folder = os.path.join(DATASET_PATH, split)
 
         if not os.path.isdir(split_folder):
             continue
@@ -54,6 +67,7 @@ def populate_database(conn):
 
                     filesize = os.path.getsize(filepath)
 
+                    # Inserts values into the columns of the mri_images table
                     cursor.execute("""INSERT INTO mri_images (
                         filename, 
                         filepath, 
@@ -61,18 +75,20 @@ def populate_database(conn):
                         split, 
                         width, 
                         height, 
-                        filesize) VALUES (?, ?, ?, ?, ?, ?, ?)""", (
+                        filesize
+                        ) 
+                        VALUES (?, ?, ?, ?, ?, ?, ?)""", (
                             filename,
                             filepath,
                             diagnosis,
                             split,
                             width,
                             height,
-                            filesize)
+                            filesize
+                            )
                         )
 
     conn.commit()
-    conn.close()
 
 
 
