fc6e7b883bfcec8eb696f80982e20f5b5b30d639
Markov134 <stephanmarokvski@gmail.com>
2026-07-18 01:02:05 +0200

    Created a notebook file and started developing it. Made a preprocessing python file for image processing. Updated .gitignore.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,6 @@
 from database import *
 
 def main():
-    connection = connect_db()
-
-    #create_tables(connection)
-    #print('done')
-    populate_database(connection)
-    print('done')
-    return None
 
 if __name__ == "__main__":
     main()
